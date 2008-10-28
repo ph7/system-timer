b7ad1f7fc10ba41515ed00976fe15986a3e807fc
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2008-10-28 14:38:25 -0700

    Froze dust and mocha

--- test/system_timer_test.rb
+++ test/system_timer_test.rb
@@ -1,7 +1,7 @@
  $: << File.dirname(__FILE__) + '/../lib'
  $: << File.dirname(__FILE__) + '/../ext/system_timer'
- $: << File.dirname(__FILE__) + "/../../../vendor/gems/dust-0.1.4/lib"
- $: << File.dirname(__FILE__) + "/../../../vendor/gems/mocha-0.5.3/lib"
+ $: << File.dirname(__FILE__) + "/../vendor/gems/dust-0.1.6/lib"
+ $: << File.dirname(__FILE__) + "/../vendor/gems/mocha-0.9.1/lib"
 require 'test/unit'
 require 'system_timer'
 require 'dust'
